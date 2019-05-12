994c695d366e4e59c4f9fd96ee725dbe11979dca
Alex Wilkinson <awilk901@gmail.com>
2019-05-12 08:55:06 -0500

    Updates

---
+++ precache-manifest.0472c5aac4d2c33bfc830ff87f659b0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3f1bbf553ce80f530a919e317533b7",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "f67a1c7095d30f9569dc",
+    "url": "/pokemon/static/css/main.9cc8a2f1.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokemon/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "f67a1c7095d30f9569dc",
+    "url": "/pokemon/static/js/main.5f26b286.chunk.js"
+  },
+  {
+    "revision": "b04917ec47d1c084d1e9",
+    "url": "/pokemon/static/js/runtime~main.0909cbae.js"
+  }
+]);
